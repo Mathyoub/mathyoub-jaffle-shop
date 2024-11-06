0c4efe553a6750e5345cc6aaf2e439f9ea0d33c0
Matthew Beall <matthew@beall.org>
2024-11-06 11:48:17 -0700

    Update stg_supplies.sql

--- models/staging/stg_supplies.sql
+++ models/staging/stg_supplies.sql
@@ -17,7 +17,7 @@ renamed as (
         sku as product_id1,
 
         ---------- text
-        UPPER(name) as supply_name,
+        LOWER(name) as supply_name,
         name as supply_name1,
         name as supply_name2,
         name as supply_name3,
